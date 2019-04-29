c84e12965adb1cbde347d17aa35972cd27391945
TinaBryan <tina@eltoro.com>
2019-04-29 17:32:52 -0400

    switched to regex thanks to gabe.  it is filtering the events.  next will be to get the details

--- fest_scrape.py
+++ fest_scrape.py
@@ -1,15 +1,21 @@
-from bs4 import BeautifulSoup
+#
+# from bs4 import BeautifulSoup
+import re
 import requests
 import csv
 
 source = requests.get('https://visitindiana.com/events?sort=distance-location&cat=5%2C7%2C8&miles=50&location=40203').text
 
-soup = BeautifulSoup(source, 'lxml')
+titleRE = re.compile(r"<h3><a href=\"events\/(?:.*?)\"><span itemprop=\"name\">(.*?)<\/span><\/a><\/h3>")
+
+# soup = BeautifulSoup(source, 'lxml')
 
 # csv_file = open('festival_in_19.csv', 'w')
 # csv.writer = csv.writer(csv_file)
 # csv.writer.writerow(['event', 'local', 'date'])
 
-event = soup.find('div', class_="content")
+# event = soup.find_all('div', class_="content")
 
-print(event.prettify())
+titles = re.findall(titleRE, source)
+for title in titles:
+    print(title)
